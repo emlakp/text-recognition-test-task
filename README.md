a303e3b223515c6d7c2beecb6787b860149591ef
Emil Akopyan <emil132435@gmail.com>
2023-12-29 15:28:35 +0100

    first

--- README.md
+++ README.md
@@ -1,25 +1,3 @@
-## Please, fork this repo first!
+I used EasyOCR together with some image processing techniques which must hopefully improve any OCR systems performance. 
 
-# Instructions
-
-- Your goal is to implement a model using any python library
-that will recognize the text from the picture as accurate as possible.
-
-- The dataset contained in `data/public_data` consist of images with English/Chinese texts
-and `info.json` file with all ground truth texts.
-
-- You should implement class located in `ocr_model.py`. Feel free to write any code you want,
-but keep in mind that the model should match `OCRModel` interface, so I will be able to easily test it.
-You can check your recognition quality on public data with `main.py` script.
-
-- Your grade would depend on both the accuracy of your model
-and the quality of your code.
-
-  -  To measure your accuracy I will download your fork and 
-run your model on the secret version of dataset (with another, but conceptually similar screenshots).
-
-
-Good luck! And feel free to text me via email if you have any questions:
-
-[aleksandr.fedotov@jetbrains.com](aleksandr.fedotov@jetbrains.com)
-
+I suspect that the performance I obtained (0.89) is roughly the best one can obtain without fine-tuning anything. For finetuning, quite specific dataset must be produced, namely screenshoots with natural language and code in english and chinese. It must not be too difficult to produce it synthetically, and will boost the performance. 
--- text_recognition/ocr_model.py
+++ text_recognition/ocr_model.py
@@ -1,4 +1,22 @@
 from pathlib import Path
+import easyocr
+from PIL import Image, JpegImagePlugin
+import numpy as np
+import cv2
+
+def convert_to_300_dpi(image: JpegImagePlugin):
+
+    """
+        This method takes an image file as input converts its DPI to 300.
+
+        :param image: The image object.
+    """
+
+    # Set the DPI to 300
+    img = img.convert('RGB')  # Convert to RGB mode if the image is not in this mode already
+    img = img.resize((int(img.width * 300 / img.info['dpi'][0]), int(img.height * 300 / img.info['dpi'][1])), resample=Image.LANCZOS)
+    img.info['dpi'] = (300, 300)
+    return img
 
 
 class OCRModel:
@@ -9,4 +27,29 @@ class OCRModel:
         :param image: The path to the image file.
         :return: The recognized text from the image.
         """
-        return 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        
+        reader = easyocr.Reader(['ch_sim','en'], gpu = True) 
+
+        im = Image.open(image)
+        im = np.array(convert_to_300_dpi(im))
+
+
+        # Converting the image to grey-scale
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        # Blurring it to reduce noise
+        blur = cv2.GaussianBlur(gray, (3,3), 0)
+        thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+        #inverting the image
+        invert = 255 - thresh
+        im = invert
+
+
+
+        result = reader.readtext(im)
+
+        text = []
+        for det in result:
+            text.append(det[1])
+
+        return ' '.join(text)
